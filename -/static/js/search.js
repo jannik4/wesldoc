0c08ba1f3fdc6e3071d8e0944c362464e0a55329
jannik4 <50554200+jannik4@users.noreply.github.com>
2025-04-04 18:27:45 +0000

    Deploying to gh-pages from @ jannik4/wesl_docs@8f975ffcfb32b0c8c27490b30a38de15302eef7f 🚀

---
+++ -/static/js/search.js
@@ -0,0 +1,118 @@
+window.DOCS_INNER_CONTENT = document.getElementById("innerContent").innerHTML;
+
+onUrlChange();
+window.addEventListener('popstate', function (event) {
+    onUrlChange();
+});
+
+function onUrlChange() {
+    var query = document.getElementById("search").value;
+
+    if ('URLSearchParams' in window) {
+        var searchParams = new URLSearchParams(window.location.search);
+        var param = searchParams.get("search") || "";
+        if (param !== query) {
+            document.getElementById("search").value = param;
+            search();
+        }
+    }
+}
+
+function searchDebounced() {
+    if (window.DOCS_SEARCH_DEBOUNCE) {
+        clearTimeout(window.DOCS_SEARCH_DEBOUNCE);
+    }
+    window.DOCS_SEARCH_DEBOUNCE = setTimeout(search, 400);
+}
+
+function search() {
+    var innerContentElement = document.getElementById("innerContent");
+    var query = document.getElementById("search").value;
+    var items = window.DOCS_ITEMS || [];
+
+    if ('URLSearchParams' in window) {
+        var searchParams = new URLSearchParams(window.location.search);
+        if ((searchParams.get("search") || "") !== query) {
+            if (query === "") {
+                searchParams.delete("search");
+            } else {
+                searchParams.set("search", query);
+            }
+
+            var searchParamsString = searchParams.toString();
+            if (searchParamsString === "") {
+                history.pushState(null, '', window.location.pathname);
+            } else {
+                history.pushState(null, '', window.location.pathname + '?' + searchParamsString);
+            }
+        }
+    }
+
+    if (query === "") {
+        innerContentElement.innerHTML = window.DOCS_INNER_CONTENT;
+        return;
+    }
+
+    var itemsFiltered = items.filter(function (item) {
+        return item.name.toLowerCase().includes(query.toLowerCase());
+    });
+
+    innerContentElement.innerHTML = "";
+    var itemList = document.createElement("ul");
+    itemList.className = "item-list item-list-bordered";
+    for (var i = 0; i < itemsFiltered.length; i++) {
+        itemList.appendChild(createItemElement(itemsFiltered[i]));
+    }
+    innerContentElement.appendChild(itemList);
+}
+
+function createItemElement(item) {
+    var className = "";
+    switch (item.kind) {
+        case "Module":
+            className = "module";
+            break;
+        case "Constant":
+            className = "const";
+            break;
+        case "GlobalVariable":
+            className = "var";
+            break;
+        case "Struct":
+            className = "struct";
+            break;
+        case "Function":
+            className = "fn";
+            break;
+        case "TypeAlias":
+            className = "type";
+            break;
+        default: break;
+    }
+
+    var linkElement = document.createElement("a");
+    linkElement.href = window.DOCS_THIS_PACKAGE.root + item.url;
+
+    for (var i = 0; i < item.path.length; i++) {
+        var segment = document.createElement("span");
+        segment.className = "path";
+        segment.innerText = item.path[i];
+        linkElement.appendChild(segment);
+
+        var segment = document.createElement("span");
+        segment.className = "path";
+        segment.innerText = "::";
+        linkElement.appendChild(segment);
+
+        linkElement.appendChild(document.createElement("wbr"));
+    }
+
+    var itemElement = document.createElement("span");
+    itemElement.className = className;
+    itemElement.innerText = item.name;
+    linkElement.appendChild(itemElement);
+
+    var listElement = document.createElement("li");
+    listElement.appendChild(linkElement);
+    return listElement;
+}
---
+++ -/static/js/theme.js
@@ -0,0 +1,19 @@
+function currentTheme() {
+    const userPrefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+    return localStorage.getItem("DOCS_theme") || (userPrefersDarkTheme.matches ? "dark" : "light");
+}
+
+function updatePage(theme) {
+    document.querySelector("html").setAttribute("data-theme", theme);
+    const button = document.getElementById("toggleTheme");
+    button.textContent = theme === "dark" ? "🌣" : "☾";
+    button.setAttribute("aria-label", theme === "dark" ? "Switch to light mode" : "Switch to dark mode");
+}
+
+function toggleTheme() {
+    const newTheme = currentTheme() === "dark" ? "light" : "dark";
+    localStorage.setItem("DOCS_theme", newTheme);
+    updatePage(newTheme);
+}
+
+updatePage(currentTheme());
