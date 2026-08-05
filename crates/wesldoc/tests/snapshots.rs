bfcf31227ab05cd44ea992c2f9d5b4e991409683
Jannik Obermann <jannik.obermann@gmail.com>
2026-08-05 17:56:10 +0200

    Add snapshot test

---
+++ crates/wesldoc/tests/snapshots.rs
@@ -0,0 +1,44 @@
+use clap::Parser;
+use dirsnap::Assert;
+use wesldoc::{Args, Result};
+
+#[test]
+fn example_packages() -> Result<()> {
+    snapshot_test(
+        "example_packages",
+        &[
+            "../../example_packages/primitives",
+            "../../example_packages/math_utils",
+            "../../example_packages/pbr",
+        ],
+    )
+}
+
+fn snapshot_test(name: &str, packages: &[&str]) -> Result<()> {
+    let tmp_dir = tempfile::tempdir()?;
+
+    for package in packages {
+        build_package(package, &tmp_dir)?;
+    }
+
+    Assert::new()
+        .with_action_env_var("SNAPSHOTS")
+        .ignore("-/static")
+        .eq(format!("tests/snapshots/{name}"), tmp_dir.path());
+
+    Ok(())
+}
+
+fn build_package(package_path: &str, tmp_dir: &tempfile::TempDir) -> Result<()> {
+    let output_path = tmp_dir.path();
+
+    Args::parse_from([
+        "wesldoc",
+        package_path,
+        "--output",
+        output_path.to_str().unwrap(),
+    ])
+    .run()?;
+
+    Ok(())
+}
