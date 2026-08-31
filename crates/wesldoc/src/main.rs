cdeeda6b46dfba870ebbbca23fa4781367cc5d0f
Jannik Obermann <jannik.obermann@gmail.com>
2026-08-31 13:00:39 +0200

    Use human_panic

--- crates/wesldoc/src/main.rs
+++ crates/wesldoc/src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 
 fn main() -> anyhow::Result<()> {
+    human_panic::setup_panic!();
+
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn")).init();
     wesldoc::Args::parse().run()?;
 
